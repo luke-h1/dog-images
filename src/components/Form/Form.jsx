75f9e2a6cfee2f65c60628cda3790efcc8c9e820
luke-h1 <luke.howsam@yahoo.com>
2020-11-08 17:42:04 +0000

    updates

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,29 +1,43 @@
 import React, { useState } from 'react';
 import './Form.scss';
 import axios from 'axios';
-const Form = () => {
+import PropTypes from 'prop-types';
+import Loading from '../Loading/Loading';
+const Form = ({ title }) => {
   const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const onSubmit = (e) => {
     e.preventDefault();
-    if(text === '' | text === null) { 
-      return; 
-    }else { 
+    if (text === '' || text === null) {
+      return;
+    } else {
       searchDogs(text);
       setText('');
     }
   };
 
-  const searchDogs = async (text) => { 
-    const BASE_URL = `https://dog.ceo/api/breed/${text}/images/random/5`
-    const res = await axios.get(`${BASE_URL}`)
-    console.log(res.data)
-  }
-  const onChange = (e) => setText(e.target.value)
+  const searchDogs = async (text, i) => {
+    setLoading(true);
+    const BASE_URL = `https://dog.ceo/api/breed/${text}/images/random/5`;
+    const res = await axios.get(`${BASE_URL}`);
+    console.log(res.data);
+    const results = res.data.message.map((dog) => (
+      <img src={dog} className="doggo-image" alt="dogs" />
+    ));
+
+    setData(results);
+  };
+  const onChange = (e) => setText(e.target.value);
+
+
+
+  if(loading && <Loading />) 
 
   return (
-    <>
+    <Load>
       <div className="form-wrapper">
-        <h2 className="form-title">Enter your favourite dog !</h2>
+        <h2 className="form-title">{title}</h2>
         <form className="form" onSubmit={onSubmit}>
           <input
             type="text"
@@ -31,13 +45,21 @@ const Form = () => {
             placeholder="search dogs! 🐶"
             value={text}
             onChange={onChange}
-            className='form-input'
+            className="form-input"
           />
-          <input type='submit' value='Submit' className='btn'/> 
+          <input type="submit" value="Submit" className="btn" />
         </form>
-        {/* add a clear button here */}
+        <div className="doggo-container">{data}</div>
       </div>
-    </>
+    </Loading>
   );
+};
+
+Form.defaultProps = {
+  title: 'Enter your favourite dog !',
+};
+
+Form.propTypes = {
+  title: PropTypes.string.isRequired,
 };
 export default Form;
